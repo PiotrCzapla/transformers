f54f7393d7e790f3a3ffadc87e3cbe4ad7967f49
Piotr Czapla <piotr.czapla@n-waves.com>
2019-10-04 12:05:08 +0000

    Move sotabench files to sotabench/

--- sotabench.py
+++
@@ -1,8 +0,0 @@
-from sotabench_scripts import language_modelling as lm
-from sotabench_scripts import squad
-def main():
-   squad.main()
-   lm.main()
-   
-if __name__ == '__main__':
-    main()
---
+++ sotabench/sotabench.py
@@ -0,0 +1,8 @@
+from impl import language_modelling as lm
+from impl import squad
+def main():
+   squad.main()
+   lm.main()
+   
+if __name__ == '__main__':
+    main()
